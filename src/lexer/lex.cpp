d4cbf8944c9cf7fbc05f7e308e685d09b7cbebc3
will chandler <jaskamiin@gmail.com>
2024-09-10 20:32:20 -0400

    add some lexer stuff - char classes/etc

--- src/lexer/lex.cpp
+++ src/lexer/lex.cpp
@@ -1,45 +1,43 @@
 #include "lex.h"
 
 
-lex::Lexer Lexer() { }
+lex::Lexer::Lexer() { }
 
-lex::Lexer Lexer(const std::string& line) {
-    currentState = LexerState::START;
-}
+lex::Lexer::Lexer(const std::string& line) : current_state(LexerState::START) {}
 
-bool lex::Lexer::ready() {
-    return this->ready;
-}
+// bool lex::Lexer::ready() {
+//     return this->ready;
+// }
 
 void lex::Lexer::consume(const char c) {
     this->current_char = c;
-
-    uint8_t chclass = classify(c);
+    //uint8_t chclass = classify(c);
     
-    switch (chclass) {
-        case 0x20:
-            this->current_state = LexerToken::WS;
-            break;
-        case '0':
-            this->current_state = LexerToken::NUM_Z;
-            break;
-        case '1':
-            this->current_state = LexerToken::NUM_NZ;
-            break;
+}
+
+
+bool lex::IsInCClass(char c, uint8_t cclass) {
+    bool inClass = true;
+    while (cclass && inClass) {
+        inClass &= CheckClass[(cclass & (-cclass))](c);
+        cclass &= (cclass - 1);
     }
+    return inClass;
 }
 
-Token lex::Lexer::emit() {
+// Token lex::Lexer::emit() {
     
-}
+// }
 
 
 // collapse some characters into classifications
-inline uint8_t lex::Lexer::classify() {
+// TODO: add logic to collapse based on previous token (e.g. symbols in strings)?
+uint8_t lex::Lexer::classify(const char c) {
     uint8_t chclass = static_cast<uint8_t>(c);
     if (chclass == 0x9 || chclass == 0xA || chclass == 0x20) {
         chclass = 0x20;
     }
+    
     if (chclass > '0' && chclass <= '9') {
         chclass = static_cast<uint8_t>('1');
     }
--- src/lexer/lex.h
+++ src/lexer/lex.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <string>
 #include <unordered_map>
+#include <cstdint>
+#include <vector>
+
 
 namespace lex {
 enum class LexerState : uint8_t {
@@ -54,35 +57,200 @@ enum class LexerState : uint8_t {
     SYM_PARO    = 0x92,  //  (
     SYM_PARC    = 0x93,  //  )
     SYM_BKTO    = 0x94,  //  [
-    SYM_BKTC    = 0x95,  //  ]
+    SYM_BKTC    = 0x95   //  ]
 };
 
+const std::unordered_map<LexerState, std::unordered_map<char, LexerState>> TransitionTable {
+    {LexerState::START,
+     {{{' ', LexerState::WS},
+       {'0', LexerState::NUM_Z},
+       {'1', LexerState::NUM_NZ},
+       {'A', LexerState::IDENT},
+       {'\"', LexerState::SYM_DQUO},
+       {'.', LexerState::SYM_DOT},
+       {'@', LexerState::SYM_AT},
+       {':', LexerState::SYM_COL},
+       {'~', LexerState::SYM_TL},
+       {'+', LexerState::SYM_PM},
+       {'-', LexerState::SYM_PM},
+       {'?', LexerState::SYM_QUES},
+       {'/', LexerState::SYM_FS},
+       {'<', LexerState::SYM_XT},
+       {'>', LexerState::SYM_XT},
+       {'=', LexerState::SYM_EQ},
+       {'!', LexerState::SYM_EX},
+       {'{', LexerState::SYM_BRCO},
+       {'}', LexerState::SYM_BRCC},
+       {'(', LexerState::SYM_PARO},
+       {')', LexerState::SYM_PARC},
+       {'[', LexerState::SYM_BKTO},
+       {']', LexerState::SYM_BKTC}}}},  
+    
+    {LexerState::WS,
+     {{' ', LexerState::WS}}},
 
-std::unordered_map<uint8_t, LexerState> StateMap {
-    {0x20, LexerState::WS},
-    {0x30, LexerState::NUM_Z},
-    {0x31, LexerState::NUM_NZ},
-    {0x41, LexerState::IDENT},
+    {LexerState::NUM_Z,
+     {{'0', LexerState::NUM_Z},
+      {'1', LexerState::NUM_NZ},
+      {'.', LexerState::NUM_FP},
+      {'B', LexerState::NUM_B2_P},
+      {'b', LexerState::NUM_B2_P},
+      {'O', LexerState::NUM_B8_P},
+      {'o', LexerState::NUM_B8_P},
+      {'X', LexerState::NUM_B16_P},
+      {'x', LexerState::NUM_B16_P},
+      {'f', LexerState::NUM_PF}}},
+
+    {LexerState::NUM_NZ,
+     {{'0', LexerState::NUM_NZ},
+      {'1', LexerState::NUM_NZ},
+      {'.', LexerState::NUM_FP},
+      {'f', LexerState::NUM_PF}}}, 
+    
+    {LexerState::NUM_FP,
+     {{'0', LexerState::NUM_FP},
+      {'1', LexerState::NUM_FP},
+      {'f', LexerState::NUM_PF}}},    
+        
+    // for now we'll delay validation of different bases, until classification improves
+    {LexerState::NUM_B2_P,
+     {{'0', LexerState::NUM_B2},
+      {'1', LexerState::NUM_B2}}},
+    
+    {LexerState::NUM_B2,
+     {{'0', LexerState::NUM_B2},
+      {'1', LexerState::NUM_B2}}},
+    
+    {LexerState::NUM_B8_P,
+    {{'0', LexerState::NUM_B8},
+     {'1', LexerState::NUM_B8}}},
+    
+    {LexerState::NUM_B8,
+    {{'0', LexerState::NUM_B8},
+     {'1', LexerState::NUM_B8}}},
+    
+    {LexerState::NUM_B16_P,
+     {{'0', LexerState::NUM_B16},
+      {'1', LexerState::NUM_B16},
+      {'A', LexerState::NUM_B16}}},
+    
+    {LexerState::NUM_B16,
+     {{'0', LexerState::NUM_B16},
+      {'1', LexerState::NUM_B16},
+      {'A', LexerState::NUM_B16}}},
+    
+    {LexerState::SYM_DOT,
+     {{'0', LexerState::NUM_FP},
+      {'1', LexerState::NUM_FP},
+      {'>', LexerState::CHANNEL}}},
+    
+    {LexerState::SYM_AT,
+    {{'!', LexerState::SIGNAL},
+        {'=', LexerState::OVERLOAD},
+        {'>', LexerState::CHANNEL}}},
+    
+    {LexerState::SYM_TL,
+    {{'>', LexerState::CHANNEL},
+     {'<', LexerState::CHANNEL}}},
+    
+    {LexerState::SYM_COL,
+    {{'A', LexerState::CASE},
+     {'0', LexerState::CASE},
+     {'1', LexerState::CASE}}},
+
+    {LexerState::CASE,
+     {{'A', LexerState::CASE},
+      {'0', LexerState::CASE},
+      {'1', LexerState::CASE},
+      {'>', LexerState::CHANNEL}}},
+
+    {LexerState::SYM_QUES,
+     {{'>', LexerState::CHANNEL}}},
+
+    {LexerState::SYM_FS,
+     {{'/', LexerState::COMMENT},
+      {'>', LexerState::CHANNEL}}},
+
+    {LexerState::SYM_PM,
+     {{'>', LexerState::CHANNEL}}},
+    
+    {LexerState::SYM_SET,
+     {{'=', LexerState::SYM_EQ}}},
+
+    {LexerState::SYM_EX,   {}},
+    {LexerState::NUM_PF,   {}},
+    {LexerState::SYM_HASH, {}},
+    {LexerState::SYM_DOLL, {}},
+    {LexerState::SYM_MOD,  {}},
+    {LexerState::SYM_UP,   {}},
+    {LexerState::SYM_AND,  {}},
+    {LexerState::SYM_STAR, {}},
+    {LexerState::SYM_SEMI, {}},
+    {LexerState::SYM_COM,  {}},
+    {LexerState::SYM_XT,   {}},
+    {LexerState::SYM_PIPE, {}},
+    {LexerState::SYM_EQ,   {}},
+    {LexerState::SYM_TICK, {}},
+    {LexerState::SYM_PARO, {}},
+    {LexerState::SYM_PARC, {}},
+    {LexerState::SYM_BRCO, {}},
+    {LexerState::SYM_BRCC, {}},
+    {LexerState::SYM_BKTO, {}},
+    {LexerState::SYM_BKTC, {}},
+    {LexerState::COMMENT,  {}},
+    {LexerState::CHANNEL,  {}},
+    {LexerState::STR,      {}}
+};
+
+//enum class CharacterClass : uint8_t {};
+
+constexpr std::unordered_map<std::string, uint8_t> CharacterClass {
+    {"",          0x00},
+    {"A-Z",       0x01},
+    {"a-z",       0x02},
+    {"A-Za-z",    0x03},
+    {"0-9",       0x04},
+    {"A-Za-z0-9", 0x07},
+    {"SP",        0x08},
+    {"TAB",       0x10},
+    {"NL",        0x20},
+    {"WS",        0x00},
+    {"_",         0x00},
+    {"SYM",       0x00},
     
 };
 
+const std::unordered_map<uint8_t, std::function<bool(char)>> CheckClass {
+    {0x01, [](char c){ return (c >= 'A' && c <= 'Z'); }},
+    {0x02, [](char c){ return (c >= 'a' && c <= 'z'); }},
+    {0x04, [](char c){ return (c >= '0' && c <= '9'); }},
+    {0x08, [](char c){ return c == ' '; }},
+    {0x10, [](char c){ return c == '\t'; }},
+    {0x20, [](char c){ return c == '\n'; }},
+    {0x40, [](char c){ return c == '_'; }},
+    {0x80, [](char c){ return (
+        (c >= ' ' && c < '0') || (c > '9' && c < 'A') ||
+        (c > 'Z' && c < 'a') || (c > 'z' && c < 127)); }}
+};
+
+
+bool IsInClass(char, uint8_t);
 
 class Lexer {
     bool ready;
     char current_char;
     LexerState current_state;
-    std::vector<char> currentLexeme;
-
-    uint8_t classify();
+    std::vector<char> currentLexeme;    
+    uint8_t classify(const char c);
     
-public:
+  public:
     Lexer();
     Lexer(const char);
     Lexer(const std::string&);
     
-    bool ready() const;
+    //bool ready() const;
     void consume(const char);
-    Token emit();
+    //Token emit();
 };
 
 }
