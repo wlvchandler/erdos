4bdec6e2fea4702c44e77a596f447a08c6ede097
will chandler <jaskamiin@gmail.com>
2024-09-10 20:34:01 -0400

    invert classifying logic

--- src/lexer/lex.cpp
+++ src/lexer/lex.cpp
@@ -1,18 +1,12 @@
 #include "lex.h"
 
 
-lex::Lexer::Lexer() { }
-
+lex::Lexer::Lexer() : current_state(LexerState::Start) { }
 lex::Lexer::Lexer(const std::string& line) : current_state(LexerState::START) {}
-
-// bool lex::Lexer::ready() {
-//     return this->ready;
-// }
 
 void lex::Lexer::consume(const char c) {
     this->current_char = c;
-    //uint8_t chclass = classify(c);
-    
+    //....
 }
 
 
@@ -25,25 +19,14 @@ bool lex::IsInCClass(char c, uint8_t cclass) {
     return inClass;
 }
 
-// Token lex::Lexer::emit() {
-    
-// }
-
-
 // collapse some characters into classifications
 // TODO: add logic to collapse based on previous token (e.g. symbols in strings)?
 uint8_t lex::Lexer::classify(const char c) {
-    uint8_t chclass = static_cast<uint8_t>(c);
-    if (chclass == 0x9 || chclass == 0xA || chclass == 0x20) {
-        chclass = 0x20;
-    }
-    
-    if (chclass > '0' && chclass <= '9') {
-        chclass = static_cast<uint8_t>('1');
-    }
-    
-    if ((chclass >= 'A' && chclass <= 'Z') || (chclass >= 'a' && chclass <= 'z') || chclass == '_') {
-        chclass = static_cast<uint8_t>('A'); 
+    uint8_t chclass = 0;
+    for (const auto& [bit, check] : CheckClass) {
+        if (check(c)) {
+            chclass |= bit;
+        }
     }
     return chclass;
 }
